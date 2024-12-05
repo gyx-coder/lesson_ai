1739c8fc2aab4833f955dba322b34d93167c1dde
gyx-coder <2994047619@qq.com>
2024-12-05 16:35:40 +0800

    二叉树遍历和Object

---
+++ algorithm/treeOrder/6.js
@@ -0,0 +1,19 @@
+const key = 'abc123'
+let points = 50
+let winner = false
+
+const person = {
+    name: '张三',
+    age: 18
+
+}
+// 函数是对象，静态的方法 属于类的
+// const wes = Object.assign({}, person)
+// console.log(wes);
+// wes.age = 30
+// console.log(wes, person);
+// 不可写writable
+const wes = Object.freeze(person)
+person.age = 21
+wes.hometown = '萍乡'
+console.log(wes);

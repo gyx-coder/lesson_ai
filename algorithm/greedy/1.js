8091b45e0c8d7fc6a788d7c713778b5e98b2902f
gyx-coder <2994047619@qq.com>
2024-12-17 16:27:41 +0800

    贪心和动态规划区别

--- algorithm/greedy/1.js
+++ algorithm/greedy/1.js
@@ -1,8 +1,20 @@
 // - 贪心策略
 // - 局部最优解
 // - 全局最优解
-function coinChangeGreedy(coins,amt){
-
+function coinChangeGreedy(coins, amt) {
+  // 假设coins 是有序的 升序
+  let i = coins.length - 1
+  let count = 0
+  while (amt > 0) { // 还要找零
+    while (i > 0 && coins[i] > amt) {
+      i--
+    }
+    amt = amt - coins[i]
+    count++
+  }
+  return amt === 0 ? count : -1
 }
-
-coinChangeGreedy([1,20,50],60)
+// 某种组合是适合贪心的
+coinChangeGreedy([1, 5, 10, 20, 50, 100], 131)
+// 贪心策略不适合
+// coinChangeGreedy([1,20,50],60)

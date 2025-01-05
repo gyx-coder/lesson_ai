59e02e2a822a1863834672dd2c5d3a9d5f9bf133
gyx-coder <2994047619@qq.com>
2025-01-05 23:58:32 +0800

    es6 promise 和手写Ajax

---
+++ js/ajax_promise/2.js
@@ -0,0 +1,22 @@
+// 实例化时，传递函数，里面装耗时性任务
+const p = new Promise((resolve,regect) => { // executor 执行器
+  console.log("333") // 同步任务
+  setTimeout(() => { // 异步任务 event loop
+    console.log('222')
+    // resolve()
+    // regect()
+    resolve('BMW325')
+  }, 1000)
+})
+// 异步任务的执行顺序控制的话 用promise 
+console.log(p.__proto__,p)
+p.then(() => {
+  // 等到executor 异步任务执行完后，再执行then里面的函数
+  console.log('111')
+  console.log(p)
+  console.log(data);
+  
+})
+  .catch(()=>{
+    console.log('error')
+  })

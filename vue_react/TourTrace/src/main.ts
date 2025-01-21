4686ae37860eb3a653a5aed2562d6b71e65f023b
gyx-coder <2994047619@qq.com>
2025-01-21 13:49:12 +0800

    TourTrace

--- vue_react/TourTrace/src/main.ts
+++ vue_react/TourTrace/src/main.ts
@@ -1,5 +1,10 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 import './style.css'
+import router from './router/index.ts'
 import App from './App.vue'
+const app = createApp(App)
 
-createApp(App).mount('#app')
+app.use(createPinia()) // pinia 启用
+app.use(router)
+app.mount('#app')

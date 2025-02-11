f95175fa21424c1f0a897047bffe4ad821a6c64c
gyx-coder <2994047619@qq.com>
2025-02-11 22:50:10 +0800

    koa

---
+++ koa/demos/5.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+// 使用fs.promises访问 promise-based 文件系统方法
+const fsPromises = fs.promises; // js 异步 prom
+const Koa = require('koa');
+const app = new Koa();
+
+const main = async ctx => {
+  ctx.response.type = 'html';
+  // 回调函数来解决异步
+  console.log(fs.readFile('./index.html',()=>{}),fsPromises.readFile('./index.html'))
+  ctx.response.body = await fsPromises.readFile('./index.html');
+}
+
+app.use(main);
+app.listen(3000);

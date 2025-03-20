265769b3b2216481aad103f93ebc5f1c5070ba9e
gyx-coder <2994047619@qq.com>
2025-03-20 16:50:55 +0800

    shadcn

---
+++ interview/chunzhao/react/react-ts-search/README.md
@@ -0,0 +1,20 @@
+- react ui 组件库
+  shadcn-ui
+- react-router-dom
+  Home Search 
+- axios api 接口 
+- store 
+- types/articles.ts
+- hooks/
+- tailwindcss
+  - 原子类
+  - 几乎不需要写css，适配也方便
+  - tailwincss 语义和 AIGC 生成前端代码 更搭  v0.dev(截图->前端代码)
+
+- @types/node
+  类型申明文件
+  node 早于 typescript 
+  单独发布@types/node 类型包
+
+- pnpm dlx shadcn@latest init
+- pnpm dlx shadcn@latest add button 
---
+++ interview/chunzhao/react/react-ts-search/src/api/product.ts
@@ -0,0 +1,11 @@
+import type{ Product } from '../types/product'
+
+const _products: Product[] = [
+  { 'id': 1, 'title': 'iPad 4 Mini', 'price': 500.01, 'inventory': 2 },
+  { 'id': 2, 'title': 'H&M T-Shirt White', 'price': 10.99, 'inventory': 10 },
+  { 'id': 3, 'title': 'Charli XCX - Sucker CD', 'price': 19.99, 'inventory': 5 }
+];
+export const getProducts = async() =>{
+  await new Promise(resolve => setTimeout(resolve,1000))
+  return _products
+}

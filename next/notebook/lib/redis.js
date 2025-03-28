173ae43be923b32c9f92e82db72e23ee7ed3d70a
gyx-coder <2994047619@qq.com>
2025-03-28 23:32:48 +0800

    next.js

---
+++ next/notebook/lib/redis.js
@@ -0,0 +1,28 @@
+import Redis from "ioredis"; // redis node js client
+
+const REDIS_PASSWORD = '123456'
+
+const redis = new Redis({
+  host: 'localhost', // 或者你的 Redis 服务器地址
+  port: 6379,        // 或者你的 Redis 服务器端口
+  password: REDIS_PASSWORD,
+});
+
+// const redis = new Redis()
+// key=> value  NOSQL  JSON 非关系型数据库
+const initialData = {
+  "1702459181837": '{"title":"sunt aut","content":"quia et suscipit suscipit recusandae","updateTime":"2023-12-13T09:19:48.837Z"}',
+  "1702459182837": '{"title":"qui est","content":"est rerum tempore vitae sequi sint","updateTime":"2023-12-13T09:19:48.837Z"}',
+  "1702459188837": '{"title":"ea molestias","content":"et iusto sed quo iure","updateTime":"2023-12-13T09:19:48.837Z"}'
+}
+
+// 列表
+export async function getAllNotes(){
+  const data = await redis.hgetall('notes') 
+  // redis api collection mysql table
+  if(Object.keys(data).length == 0){
+    // 数据格式是JSON
+    await redis.hset("notes",initialData) // 插入数据
+  }
+  return await redis.hgetall("notes")
+}
---
+++ next/readme.md
@@ -0,0 +1,32 @@
+# next 
+- react *全栈*框架
+  - SSR 服务器端渲染
+    - 首屏加载快
+    - SEO
+    Next.js是强大的React全栈框架，支持SSR，首屏快且利于SEO，开发高效。
+  - vercel 
+  01.dev AI 代码生成工具
+  - 企业站
+
+- notebook 笔记本
+  - 后端 note CRUD 
+    - next.js 全栈框架
+    - redis(内存) mongodb NOSQL
+  - 前端
+    - noteList
+    - search
+    - new
+    - edit
+    - delete
+
+- 设计组件？
+  layout
+  page
+    component
+
+- 约定俗成的next
+  - alias jsconfig.json 
+  - page.js 页面
+  - layout.js 布局
+    children  vue slot 
+    - page.js 页面
